aca59249f0e7b94e038f85f68d45ce4de52b507a
sai madhan <saimadhan.k@agiratech.com>
2023-09-04 15:33:37 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <main className="grid place-items-center min-h-screen h-full">
+      <h1 className="text-5xl text-center">Your Awesome Website</h1>
+      <Button>Does something</Button>
+    </main>
+  );
+}
